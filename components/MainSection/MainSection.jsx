8249f8a2b842497688f6aaa3bf82eeff0cb32769
Maria Bystrova <17mashmallow@gmail.com>
2023-10-31 01:28:53 +0400

    add search field MainSection

--- components/MainSection/MainSection.jsx
+++ components/MainSection/MainSection.jsx
@@ -1,17 +1,34 @@
 'use client';
 
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 import s from './mainSection.module.scss';
 import Book from '../Book';
 
 export default function MainSection() {
+  const [value, setValue] = useState('');
   const allBooks = useSelector((state) => state.books);
 
   const library = allBooks.map((book) => <Book title={book.title} author={book.author} year={book.year} key={book.id} id={book.id} />);
+  const filteringResult = allBooks.filter((item) => {
+    const data = [item.title.toLowerCase(), item.author.toLowerCase(), item.year];
+    return data.includes(value.toLowerCase());
+  });
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
 
   return (
     <div className={s.root}>
-      {library}
+      <div className={s.searchField}>
+        <form className={s.searchForm}>
+          <input type="text" placeholder="Начать поиск" onChange={handleChange} />
+        </form>
+      </div>
+      <div className={s.container}>
+        {library}
+      </div>
     </div>
   );
 }
