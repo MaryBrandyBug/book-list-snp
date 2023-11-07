205db47670bc263cdd9266455dc10c63df9a0e18
Maria Bystrova <17mashmallow@gmail.com>
2023-11-08 02:21:17 +0400

    add styles to [id] page: create book picture, add data to [id] page, add new colors styles/colors.scss

--- src/app/[id]/page.jsx
+++ src/app/[id]/page.jsx
@@ -1,9 +1,28 @@
 'use client';
 
-export default function page() {
+import { useSelector } from 'react-redux';
+
+import s from './page.module.scss';
+
+export default function page({ params: { id } }) {
+  const allBooks = useSelector((state) => state.books);
+  const book = allBooks.filter((item) => item.id === Number(id))[0];
+
   return (
-    <div>
-      hello
+    <div className={s.root}>
+      <div className={s.bookBorder}>
+        <div className={s.bookContentContainer}>
+          <div className={s.authorContainer}>
+            <p>{book.author}</p>
+          </div>
+          <div className={s.titleContainer}>
+            <p>{book.title}</p>
+          </div>
+        </div>
+        <div className={s.bookSpine} />
+        <div className={s.bookPages} />
+        <p className={s.bookYear}>{book.year}</p>
+      </div>
     </div>
   );
 }
