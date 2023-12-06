9efec1809e911ef8eb9bad2d1f412b6610b332b6
Maria Bystrova <17mashmallow@gmail.com>
2023-12-07 00:00:02 +0400

    fix min year : change min year in validation file to 1900

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -4,7 +4,7 @@ export default yup.object().shape({
   title: yup.string().matches(/\S/, 'Строка не должна состоять только из пробелов').required('Обязательно для заполнения!'),
   author: yup.string().matches(/\S/, 'Строка не должна состоять только из пробелов').required('Обязательно для заполнения!'),
   year: yup.number('введите число').required('Обязательно для заполнения!').integer('Введите целое число')
-    .min(1, 'Введите значение не равное 0')
+    .min(1900, 'Год должен быть не ранее 1900')
     .max(new Date().getFullYear(), 'Этот год еще не наступил :( попробуйте еще раз')
     .test('not-starts-with-zero', 'Год не может начинаться с 0', (value) => {
       const yearAsString = value.toString().charAt(0);
